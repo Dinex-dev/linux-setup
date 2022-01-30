8c45698fe93e7efb491b38c99ed6b313608688c6
Dinex-dev <dinexsama@gmail.com>
2022-01-30 19:36:06 +0530

    Trying git config

--- script.sh
+++ script.sh
@@ -4,8 +4,16 @@
 #   echo "This script must be run as root" 1>&2
 #   exit 1
 #fi
-sudo pacman 
+
 clear
+
+echo "Enter your Github Username "
+read git_name
+echo "Enter your Github Email "
+read git_email
+echo "Enter your Github Personal token "
+read git_oken
+
 if pacman -Qs figlet > /dev/null ; then
   echo -e "#######################################################################################\n\nPrerequisites Satisfied Proceeding with installation\n"
 else
@@ -23,8 +31,13 @@ echo "Adding Chaotic-aur and Sublimetext to pacman.conf"
 #pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 #pacman-key --lsign-key FBA220DFC880C036
 #pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" >> file
-pacman -S
+#echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" >> file
+
+#pacman -Syyu git sublime-text sublime-merge vscodium vscodium-marketplace transmission-cli mpv-full-git tlauncher pamac appimagelauncher google-chrome 
+
+figlet "Setting Up Git"
+git config --global user.name "$git_name"
+git config --global user.email "$git_email"
 echo "Press Enter to exit "
 read exit
 clear
